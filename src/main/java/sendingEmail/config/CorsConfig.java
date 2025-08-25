5bacd67fc90defa585824d6fc6af1bdb209e2781
WillianRoosa <willian_roosa@icloud.com>
2025-08-25 18:44:40 -0300

    commit: ajustando o CorsConfig.java

--- src/main/java/sendingEmail/config/CorsConfig.java
+++ src/main/java/sendingEmail/config/CorsConfig.java
@@ -13,7 +13,7 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("https://willianroosa.github.io/PortfolioDev")
+                        .allowedOrigins("https://willianroosa.github.io")
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(true);
